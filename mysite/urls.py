7b83a11d473521bed0adf5b624e88a4134e84303
borys <boris.nosatiuk@gmail.com>
2022-09-18 19:02:43 +0300

    +

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,12 +17,6 @@ from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import path
 
-
-def home(request):
-    return JsonResponse({"hello": "world!"})
-
-
 urlpatterns = [
-    path("", home, name="home"),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
